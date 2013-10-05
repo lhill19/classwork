235ad45f76cfb3b41f4be8c58119ed826e5c7253
lhill19 <lhill19@vm-0.lhill19.koding.kd.io>
2013-10-05 00:24:34 +0000

    Rock. paper, scissors game

---
+++ rps.py
@@ -0,0 +1,31 @@
+import random
+computer = random.choice(['rock', 'paper', 'scissors'])
+user = raw_input()
+print "user chose", user
+print  "computer chose", computer
+
+while raw_input() != "quit"
+if user == "rock" and computer == "rock";
+     print "tie"
+elif user == 'paper' and computer == 'rock';
+      print 'computer wins'
+elif user == 'scissors and computer == 'rock';
+      print 'user wins'
+elif computer == 'paper' and user == 'rock';
+      print 'computer wins'
+elif user == 'scissors' and computer == 'rock';
+      print 'computer wins'
+elif user == 'paper' and computer == 'paper';
+      print 'tie'
+elif user == 'scissers' and computer == 'scissers';
+      print 'tie'
+elif user == 'paper' and computer == 'scissors';
+      print 'computer wins
+elif user == 'scissors' and computer == 'paper';
+      print 'user wins'
+elif computer == 'rock' and user == 'paper';
+      print 'user wins'
+elif computer == 'rock' and user == 'rock';
+      print 'tie'
+      
+      
