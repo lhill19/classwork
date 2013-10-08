0c6bc9f8062459502ae527be91a1f24dcfae5b2b
lhill19 <lhill19@vm-0.lhill19.koding.kd.io>
2013-10-08 08:04:39 +0000

    Correctin syntax rps.py

--- rps.py
+++ rps.py
@@ -4,28 +4,28 @@ user = raw_input()
 print "user chose", user
 print  "computer chose", computer
 
-while raw_input() != "quit"
-if user == "rock" and computer == "rock";
+while raw_input() != "quit":
+if user == "rock" and computer == "rock":
      print "tie"
-elif user == 'paper' and computer == 'rock';
+elif user == 'paper' and computer == 'rock':
       print 'computer wins'
-elif user == 'scissors and computer == 'rock';
+elif user == 'scissors and computer == 'rock':
       print 'user wins'
-elif computer == 'paper' and user == 'rock';
+elif computer == 'paper' and user == 'rock':
       print 'computer wins'
-elif user == 'scissors' and computer == 'rock';
+elif user == 'scissors' and computer == 'rock':
       print 'computer wins'
-elif user == 'paper' and computer == 'paper';
+elif user == 'paper' and computer == 'paper':
       print 'tie'
-elif user == 'scissers' and computer == 'scissers';
+elif user == 'scissers' and computer == 'scissers':
       print 'tie'
-elif user == 'paper' and computer == 'scissors';
+elif user == 'paper' and computer == 'scissors':
       print 'computer wins
-elif user == 'scissors' and computer == 'paper';
+elif user == 'scissors' and computer == 'paper':
       print 'user wins'
-elif computer == 'rock' and user == 'paper';
+elif computer == 'rock' and user == 'paper':
       print 'user wins'
-elif computer == 'rock' and user == 'rock';
+elif computer == 'rock' and user == 'rock':
       print 'tie'
       
       
